5b2479a8fb6c8977c6ff0dad4bfccfbf6a066f9a
err0rgod <ndgaming458@gmail.com>
2025-06-06 18:40:43 +0530

    fully working code done version 1 completed

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from PyPDF2 import PdfReader, PdfWriter
 
 
 parser = argparse.ArgumentParser(description='Protect PDF with password.')
-parser.add_argument('inputpdf',help='path to the PDF file to protect')
-parser.add_argument('outputpdf',help='path to the output PDF file')
-parser.add_argument('passwd',help='passwd to protect the PDF file')
+parser.add_argument('-i', '--inputpdf', help='path to the PDF file to protect')
+parser.add_argument('-o', '--outputpdf', help='path to the output PDF file')
+parser.add_argument('-p', '--passwd', help='passwd to protect the PDF file')
 
 args = parser.parse_args()
 
